69fbb158804f0b19b9006cde4348f1d1fb2af62d
MystericalGD <nattawin.ya@gmail.com>
2023-06-03 21:24:36 +0700

    fix: import argparse

--- run_new.py
+++ run_new.py
@@ -10,7 +10,7 @@ import pandas as pd
 import numpy as np
 import json
 import os
-import parser
+from argparse import parser
 
 def parse_args():
     model_names = ["AttentiveFP", "GAT", "GCN","FragAttentiveFP"]
